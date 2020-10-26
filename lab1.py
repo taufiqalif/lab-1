5ca9c9092a1e3d3b62c146ac4bb56e1059656e53
taufiqalif <aliftaufiq4@gmail.com>
2020-10-26 18:38:52 +0700

    program python

---
+++ lab1.py
@@ -0,0 +1,30 @@
+print("==========")
+print("BIODATA")
+print("==========")
+
+nama_lengkap = input("masukan nama lengkap: ")
+nama_panggilan = input("masukan nama panggilan: ")
+npm = input("masukan no NPM: ")
+tempat_lahir = input("tempat lahir: ")
+tanggal_lahir = input("tanggal lahir: ")
+umur = input("umur anda: ")
+telepon = input("no telepon/hp: ")
+alamat = input("alamat rumah: ")
+
+
+# output
+
+print("=============")
+print("BIODATA DIRI")
+print("=============")
+
+print("Assalamu'alaikum.")
+
+print("Let me introduce my self. My name is", nama_lengkap,
+      ", but you can call me", nama_panggilan, ". My NPM is", npm)
+print("I was born in", tempat_lahir, ", my date of birth",
+      tanggal_lahir, "and i am", umur, "year old.")
+print("I am very glad if you want to invite my house in", alamat,
+      "so, don't forget to call me before with the number", telepon)
+
+print("Thank you")
